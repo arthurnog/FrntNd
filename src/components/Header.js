04ccd76388baaae697cd57ac49a8c3521d5df8d3
arthurnog <artuts874@gmail.com>
2020-09-22 08:15:29 -0300

    meu noterbook n aguenta

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+export default function Header() {
+    return(
+        <Header>
+            <h1>Aprendendo React</h1>
+        </Header>
+    );
+}
